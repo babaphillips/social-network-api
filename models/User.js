b272e747cc06840d9803dbff2e36c8a73f9cccbe
babaphillips <amandavphillips@icloud.com>
2022-09-22 20:44:06 -0400

    User Model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please type a valid email address"],
+      match: [/^.+@(?:[\w-]+\.)+\w+$/, "Please type a valid email address"],
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of thoughts and reactions on retrieval
+UserSchema.virtual("friendCount").get(function () {
+  return this.users.reduce((total, user) => total + user.friends.length + 1, 0);
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
