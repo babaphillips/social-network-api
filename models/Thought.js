29382c21c49b32e7c9ee1b1861841cb8477c2ef3
babaphillips <amandavphillips@icloud.com>
2022-09-23 17:51:08 -0400

    Thought Model

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: [1, "Must be at least one character"],
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    //(The user that created this thought)
+    username: {
+      type: String,
+      required: true,
+    },
+    //(These are like replies)
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const ReactionSchema = new Schema {
+
+}
+
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
